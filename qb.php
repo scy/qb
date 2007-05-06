3c5642615293256eea667137137d1b23b23a5596
Tim Weber <scy@scytale.de>
2007-05-06 10:03:51 +0000

    Comments galore for qb's main logic. Functions follow soon.

--- qb.php
+++ qb.php
@@ -1,62 +1,102 @@
 <?php
 
+// Include configuration file.
 require_once('qb.cfg.php');
 
+// Define MIME types for the different templates.
 $mime = array(
 	'html'   => 'text/html',
 	'atom10' => 'application/atom+xml',
 );
 
+// Make $requri a tuple consisting of "everything before the first question
+// mark" and "everything after it". (The host fragment is NOT included.)
 $requri = explode('?', $_SERVER['REQUEST_URI'], 2);
+// Assign "everything before the first question mark" to $url.
 $url = $requri[0];
+// If $url begins with qb's base directory, remove it. (It really should.)
 if (substr($url, 0, strlen(QB_URLBASE)) == QB_URLBASE)
 	$url = substr($url, strlen(QB_URLBASE));
+// Remove duplicate slashes.
 $url = preg_replace('|/+|', '/', $url);
+// If the last character is a slash, remove it.
 if (substr($url, -1) == '/')
 	$url = substr($url, 0, -1);
+// If QB_NODOT is set, remove all dots and colons from the string for security.
 if (QB_NODOT)
 	$url = str_replace(array('.', ':'), '', $url);
 
+// Let $realpath be qb's source directory plus the requested file.
 $realpath = QB_SRC.$url;
+// Initialize the array of matches. It's two-dimensional. The first index is the
+// "created" Unix timestamp, the second one is running from 0 to n-1, where n is
+// the number of files created at that time. The actual value then is the file
+// name without suffix.
 $matches = array();
+// TODO: Can this be rewritten to work without distinction between "single file"
+// and "directory" case?
 if (is_file($realpath.QB_SUF_SRC)) {
+	// A single file has been requested. Add it to $matches.
 	$matches[qb_created($url)][] = $url;
 } elseif (is_dir($realpath)) {
+	// A directory has been requested. Initialize $scanq with the directory name
+	// and do a breadth first search.
 	$scanq = array($realpath);
-	while (count($scanq) > 0) {
+	while (count($scanq) > 0) { // As long as there are elements in the queue...
+		// Set $curdir to the first $scanq value, remove it from $scanq.
 		$curdir = array_shift($scanq);
+		// Return an array containing all the files and directories in it.
 		$scan = scandir($curdir);
 		foreach ($scan as $hit) {
 			$foo = "$curdir/$hit";
 			if ((is_file($foo)) && (substr($foo, 0 - strlen(QB_SUF_SRC)) == QB_SUF_SRC)) {
+				// It's a file and ends with qb's suffix. Remove the suffix...
 				$path = substr($foo, strlen(QB_SRC), 0 - strlen(QB_SUF_SRC));
+				// ...and add it to the matches.
 				$matches[qb_created($path)][] = $path;
 			} elseif ((is_dir($foo)) && ($hit != '.') && ($hit != '..')) {
+				// It's a directory and not "." or "..", add it to the queue.
 				$scanq[] = $foo;
 			}
 		}
 	}
 }
 
+// Choose the template to use. Default is "html".
+// TODO: Rewrite this to support all templates in $mime automatically.
 $template = 'html';
+// If there's a query string and it's "atom10", set the template accordingly.
 if ((count($requri) > 1) && ($requri[1] == 'atom10'))
 	$template = $requri[1];
 
+// If there has been at least one file found:
 if (count($matches) > 0) {
+	// Sort by date created, newest first.
 	krsort($matches);
+	// $content will be the content the page template sees.
 	$content = '';
+	// $lastmtime will contain the latest mtime. Ya Rly.
 	$lastmtime = 0;
-	foreach ($matches as $ctime=>$paths)
+	foreach ($matches as $ctime=>$paths) {
+		// Iterate through the first dimension, $ctime is the creation time,
+		// $paths an array of matches, which will be...
 		foreach ($paths as $match) {
+			// ...iterated as well. Add a single article to $content.
 			$content .= qb_buildpage($match, $template);
+			// Set $mtime to the file's mtime.
 			$mtime = filemtime(QB_SRC.$match.QB_SUF_SRC);
+			// Update $lastmtime, if necessary.
 			if ($mtime > $lastmtime)
 				$lastmtime = $mtime;
 		}
+	}
+	// Throw out a Content-type and charset.
 	header('Content-type: '.$mime[$template].'; charset=UTF-8');
+	// Set template variables "content" and "modified".
 	$meta['content'] = $content;
 	if ($lastmtime != 0)
 		$meta['modified'] = $lastmtime;
+	// Throw out the final page and be done. U can has cheezburger now.
 	echo(qb_template(QB_TPL_PAGE.'.'.$template, $meta));
 }
 
