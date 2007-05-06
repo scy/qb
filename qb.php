8a207b40bf62ea1d11bfd3983f545bdce14f663a
Tim Weber <scy@scytale.de>
2007-05-06 14:02:09 +0000

    Fully commented. Some slight code changes like adding braces and stuff.

--- qb.php
+++ qb.php
@@ -100,83 +100,145 @@ if (count($matches) > 0) {
 	echo(qb_template(QB_TPL_PAGE.'.'.$template, $meta));
 }
 
+// Generate an article from an input file (supplied without prefix or suffix).
+// $template can be set to the template to use, defaults to "html".
 function qb_buildpage($path, $template = 'html') {
+	// Set $filename to the real filename.
 	$filename = QB_SRC.$path.QB_SUF_SRC;
+	// Set $t to the contents of the file.
 	if (($t = @file_get_contents($filename)) === false) {
+		// If the file doesn't exist, return an error.
+		// TODO: This never happens, because main logic already checks for the
+		// existence of the file.
 		return (qb_template(QB_TPL_PAGE.'.'.$template, array(
 			'title'   => 'file not found',
 			'content' => '<p class=\'qb_error\'>The file ' . htmlspecialchars($path) . ' could not be found.</p>',
 		)));
 	} else {
+		// File exists. Let $page[0] be the first line, $page[1] be the rest.
 		$page = explode("\n", $t, 2);
+		// Throw all tags in the first line into $tokens.
 		preg_match_all('|<(.*):(.*)>(?!>)|U', $page[0], $tokens, PREG_SET_ORDER);
+		// Initialize $meta as empty array.
 		$meta = array();
 		foreach ($tokens as $token) {
+			// Let $k be the name of the tag, $v its value.
 			$k = $token[1]; $v = $token[2];
-			if ($k == '')
+			if ($k == '') {
+				// Special case: the <:...> tag is a shortcut for <title:...>
 				$k = 'title';
+			}
 			if ($k == 'tags') {
+				// Special case: expand the "tags" tag via a freaky regex that
+				// splits them by spaces, multiword tags can be supplied by
+				// using quotation marks. Should resemble Flickrs tag parsing
+				// logic somehow.
 				preg_match_all('%"(?! )([^"]+)(?<! )"|([^ ]+)%', $v, $tags, PREG_PATTERN_ORDER);
 				foreach ($tags[0] as $tag) {
-					$tag = str_replace('"', '', $tag);
-					if (strlen(trim($tag)) > 0)
+					// Remove quotation marks and spaces.
+					$tag = str_replace('"', '', trim($tag));
+					// If there's something left, add it to $parsedtags array.
+					if (strlen($tag) > 0)
 						$parsedtags[] = $tag;
 				}
+				// Remove duplicate values, save the result in $v.
 				$v = array_unique($parsedtags);
-				if (count($v) > 0)
+				if (count($v) > 0) {
+					// If there are any tags left, create a "spantags" template
+					// variable and put them in it. The "tags" variable stays
+					// an array. No idea what's the use in it, but whatever.
 					$meta['spantags'] = '<span class=\'tag\'>' . implode('</span> <span class=\'tag\'>', $v) . '</span>';
+				}
 			}
+			// Create a template variable with that value.
 			$meta[$k] = $v;
 		}
+		// "path" contains the URL base plus $path, double slashes removed.
 		$meta['path'] = preg_replace('|/+|', '/', QB_URLBASE . $path);
+		// "content" contains the second and all following lines.
 		$meta['content'] = $page[1];
+		// "escapedcontent" is like "content", but with escaped HTML characters.
 		$meta['escapedcontent'] = htmlspecialchars($meta['content']);
+		// "modified" is the modification date of the file.
 		$meta['modified'] = filemtime($filename);
+		// "created" is the date the file was created.
+		// FIXME: Use qb_created() instead?
 		$meta['created'] = filemtime(QB_META.$path.QB_SUF_CRE);
+		// If the two timestamps differ, set "wasmodified".
 		if ($meta['created'] != $meta['modified'])
 			$meta['wasmodified'] = $meta['modified'];
+		// Run the template, return the result.
 		return (qb_template(QB_TPL_ARTICLE.'.'.$template, $meta));
 	}
 }
 
+// Unix has no file creation times, bitches. Therefore we need an ugly hack.
 function qb_created($path) {
+	// $cfile is the "creation file" of the qb file in $path.
 	$cfile = QB_META.$path.QB_SUF_CRE;
 	if (!file_exists($cfile)) {
+		// If it doesn't exist yet, we'll create it. First set $ctime to "now".
 		$ctime = time();
+		// Then, set $mtime to the modification date of the file.
 		$mtime = filemtime(QB_SRC.$path.QB_SUF_SRC);
+		// If it has been modified before "now" (should be the case), set $ctime
+		// to $mtime.
 		if ($mtime < $ctime)
 			$ctime = $mtime;
+		// Create the directory for the $cfile (recursively), if needed.
 		if (!file_exists(dirname($cfile)))
 			mkdir(dirname($cfile), 0775, true);
+		// Create the $cfile and set its modification date, which serves as the
+		// creation date from now on.
 		touch($cfile, $ctime);
+		// Return the time we set.
 		return ($ctime);
 	} else {
+		// The cfile exists, return its modification time.
 		return (filemtime($cfile));
 	}
 }
 
+// Runs a bunch of patterns (regex=>replacement array) over a string.
 function qb_runpattern($pattern, $string) {
-	foreach ($pattern as $k => $v)
-		if (count($v) == 1)
+	foreach ($pattern as $k => $v) {
+		if (count($v) == 1) {
+			// This is to catch the "tags" variable, iirc...
 			$string = preg_replace($k, $v, $string);
+		}
+	}
 	return ($string);
 }
 
+// This is the funky template engine. Supply a template filename and template
+// variable array.
 function qb_template($template, $data) {
+	// Access the regexes from the config file.
 	global $qb_regex;
+	// $regex will hold the regexes generated from the supplied variables.
 	$regex = array();
+	// Iterate over each supplied template variable ($k) and its value ($v):
 	foreach ($data as $k => $v) {
+		// $nk is the "normalized key", making sure that template variables
+		// consist only of a-z0-9.
 		$nk = preg_replace('[^a-z0-9]', '', strtolower($k));
-		$data2[$nk] = $v;
+		// Add a regex that will replace a template tag with the value.
 		$regex['|<qb:' . $nk . ' */ *>|U'] = $v;
+		// Create regexes for the corresponding <qb:ifset:...> tags.
+		$regex['|<qb:ifset:' . $nk . ' *>(.*)</qb:ifset:' . $nk .' *>|Us'] = '$1';
 	}
-	foreach ($data2 as $k => $v)
-		$regex['|<qb:ifset:' . $k . ' *>(.*)</qb:ifset:' . $k .' *>|Us'] = '$1';
+	// Create a regex that will eat all other <qb:ifset:...> tags.
 	$regex['|<qb:ifset:[a-z0-9]+ *>.*</qb:ifset:[a-z0-9]+ *>|Us'] = '';
+	// Create a regex for the date magic.
 	$regex['|<qb:date>([0-9]+) *([^<>]+)</qb:date>|me'] = "date('\$2', \$1)";
+	// Merge the generated and the configured regexes. Configured ones overwrite
+	// generated ones.
 	$regex = array_merge($regex, $qb_regex);
+	// Pour the template file into $t.
 	$t = file_get_contents($template);
+	// Run all the patterns...
 	$t = qb_runpattern($regex, $t);
+	// ...and return the result.
 	return ($t);
 }
 
