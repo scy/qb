4dbb180b8976e64236d605d5e56ffeab00cce296
Rolf Eike Beer <dakon@sf-mail.de>
2007-05-07 07:35:27 +0000

    put tags in an <ul> instead of <span>

--- qb.php
+++ qb.php
@@ -187,7 +187,7 @@ function qb_buildpage($path, $template = 'html') {
 					// If there are any tags left, create a "spantags" template
 					// variable and put them in it. The "tags" variable stays
 					// an array. No idea what's the use in it, but whatever.
-					$meta['spantags'] = '<span class=\'tag\'>' . implode('</span> <span class=\'tag\'>', $v) . '</span>';
+					$meta['spantags'] = '<ul class=\'tag\'><li>' . implode("</li>\n<li>", $v) . '</li></ul>';
 				}
 			}
 			// Create a template variable with that value.
