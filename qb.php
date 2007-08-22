75a5df9b243b5ae6a8e2909f1c8e300bfd176039
Tim Weber <scy-git@scytale.name>
2007-08-22 15:26:41 +0200

    Added an __autoload() function to qb.php for the upcoming modular structure.

--- qb.php
+++ qb.php
@@ -1,5 +1,14 @@
 <?php
 
+// Right now, we still need the old stuff.
 require('lib/qb-0.2.php');
+
+// Class autoloader.
+function __autoload($class) {
+	// Remove suspicious characters.
+	$class = preg_replace('/[^a-zA-Z0-9]/', '', $class);
+	// Include the class, die if that fails.
+	require_once("lib/$class/$class.php");
+}
 
 ?>
