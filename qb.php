dfbd3d17ad60472b95d2c32411da984e903a82c4
Tim Weber <scy-git@scytale.name>
2007-10-28 20:52:02 +0100

    Renamed lib/libqb.php to lib/qb.php and removed it from the currently used qb.php.
    lib/qb.php will be what you'll include in your 0.5 site.
    I'm going to start working on this today and don't want to disturb running qb installations.

--- qb.php
+++ qb.php
@@ -1,9 +1,5 @@
 <?php
 
-// Some freaky hackish redirection in order to be able to write 0.3
-// while keeping 0.2 functional.
-require_once('lib/libqb.php');
-
 // Runs the 0.2 qb.
 require('lib/qb-0.2.php');
 
