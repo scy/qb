96f4b8a00219c5f8eead81d35b46c8be89a9daea
Tim Weber <scy-git@scytale.name>
2007-11-04 22:36:52 +0100

    Added $flip parameter to qbString::startsWith() to disable auto-flipping of arguments.

--- lib/qbString.php
+++ lib/qbString.php
@@ -26,9 +26,9 @@ class qbString {
 	/**
 	 * Return true if string $b starts with string $a.
 	 */
-	public static function startsWith($a, $b) {
+	public static function startsWith($a, $b, $flip = true) {
 		// $a should be the shorter one. If it isn't, make it.
-		if (strlen($a) > strlen($b))
+		if ($flip && (strlen($a) > strlen($b)))
 			list($a, $b) = array($b, $a);
 		return (substr($b, 0, strlen($a)) === $a);
 	}
