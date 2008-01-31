f5fdebad2f59d3f0aebc9d0c612c134343ba77fe
Tim Weber <scy-git@scytale.name>
2008-01-31 09:42:59 +0100

    Now including qb 0.5 into the 0.2 wrapper. The transition is advancing...

--- qb.php
+++ qb.php
@@ -1,5 +1,11 @@
 <?php
 
+// Access the 0.5 goodness.
+require('lib/qb.php');
+
+// Debug output. Comment this in to see whether URL auto-parsing works for you.
+// echo("<!--\n\tBase dir: " . qbURL::getBaseDir() . "\n\tHandler: " . qbURL::getHandler() . "\n\tBase path: " . qbURL::getBasePath() . "\n\tVFile: " . qbURL::getVFile() . "\n-->\n");
+
 // Runs the 0.2 qb.
 require('lib/qb-0.2.php');
 
